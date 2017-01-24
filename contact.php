603b4c11b80cd5ffc86d545ab3f2eda6d529ec6d
Thomas Munghono <notice4thomas@gmail.com>
2017-01-24 17:50:00 +0300

    Add files via upload

--- contact.php
+++ contact.php
@@ -12,11 +12,11 @@ if(isset($_POST['name']))
 	$email=$_POST['email'];
 	$message=$_POST['message'];
 
-	$to = "youremail@gmail.com";
-	$subject = "365Bootstrap - Testing Contact Form";
+	$to = "notice4thomas@gmail.com";
+	$subject = "1SMILE Initiative - Contact Form";
 	$message = " Name: " . $name ."\r\n Email: " . $email . "\r\n Message:\r\n" . $message;
 	 
-	$from = "365Bootstrap.com";
+	$from = "1SMILE Website";
 	$headers = "From:" . $from . "\r\n";
 	$headers .= "Content-type: text/plain; charset=UTF-8" . "\r\n"; 
 	 
@@ -30,19 +30,26 @@ if(isset($_POST['name']))
 <html lang="en">
 <head>
     <meta charset="utf-8">
-  	<title>PHOTO-EYE</title>
+  	<title>Contact 1SMILE</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Free Bootstrap Themes by 365Bootstrap dot com - Free Responsive Html5 Templates">
-    <meta name="author" content="www.365bootstrap.com">
+    <meta name="description" content="boda boda mobility motorcycle public health">
+    <meta name="author" content="www.1smile.org">
 
 	<!-- styles -->
-	
+	<link rel="shortcut icon" href="favicon.ico">
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link href="css/camera.css" rel="stylesheet">
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/menu.min.css">
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link href="css/pe-icon-7-stroke.css" rel="stylesheet" type="text/css"  />
 	
+    <link href="css/camera.css" rel="stylesheet" type="text/css" >
+	<link href="css/style.css" rel="stylesheet" type="text/css" >
+	<link href="css/menu.min.css" rel="stylesheet" type="text/css" >
+	<link href="css/roboto.css" rel="stylesheet" type="text/css" >
+	<link href="css/small-n-flat.css" rel="stylesheet" type="text/css" >
+	
+	<!-- FAB Button 1,2 -->
+    <link href="css/fab1.css" rel="stylesheet" type="text/css" >
+	<link rel="stylesheet" href="css/kc.fab.css"/>
 	<!-- js -->
 	<script src="js/modernizr.custom.97074.js"></script>
 	
@@ -60,7 +67,7 @@ if(isset($_POST['name']))
 	<button id="mm-menu-toggle" class="mm-menu-toggle">Toggle Menu</button>
 	<nav id="mm-menu" class="mm-menu">
 		<div class="mm-menu__header">
-			<h2 class="mm-menu__title">PHOTO-EYE</h2>
+			<h2 class="mm-menu__title">1SMILE</h2>
 		</div>
 		<ul class="mm-menu__items">
 			<li class="mm-menu__item">
@@ -86,43 +93,6 @@ if(isset($_POST['name']))
 		</ul>
 	</nav><!-- /nav -->
 	
-	<!-- /////////////////////////////////////////Header -->
-	<header id="intro">
-		<div class="camera_wrap camera_black_skin" id="camera_wrap_1"><!-- slideder star-->
-			<div data-thumb="images/slider-3-thumb.jpg" data-src="images/slider-3.jpg">
-				<div class="camera_caption fadeFromBottom">
-					Camera is a responsive/adaptive slideshow. <em>Try to resize the browser window</em>
-				</div>
-			</div>
-			<div data-thumb="images/slider-1-thumb.jpg" data-src="images/slider-1.jpg">
-				<div class="camera_caption fadeFromBottom">
-					It uses a light version of jQuery mobile, <em>navigate the slides by swiping with your fingers</em>
-				</div>
-			</div>
-			<div data-thumb="images/slider-2-thumb.jpg" data-src="images/slider-2.jpg">
-				<div class="camera_caption fadeFromBottom">
-					<em>It's completely free</em> (even if a donation is appreciated)
-				</div>
-			</div>
-			<div data-thumb="images/slider-4-thumb.jpg" data-src="images/slider-4.jpg">
-				<div class="camera_caption fadeFromBottom">
-					Camera slideshow provides many options <em>to customize your project</em> as more as possible
-				</div>
-			</div>
-			<div data-thumb="images/slider-5-thumb.jpg" data-src="images/slider-5.jpg">
-				<div class="camera_caption fadeFromBottom">
-					It supports captions, HTML elements and videos and <em>it's validated in HTML5</em> 
-				</div>
-			</div>
-			<div data-thumb="images/slider-6-thumb.jpg" data-src="images/slider-6.jpg">
-				<div class="camera_caption fadeFromBottom">
-					  Different color skins and layouts available, <em>fullscreen ready too</em>
-				</div>
-			</div>
-		</div><!-- slideder end-->
-	</header>
-	<!-- Header -->
-	
 	
 	<!-- /////////////////////////////////////////Content -->
 	<div id="page-content" class="sub-page">
@@ -134,8 +104,7 @@ if(isset($_POST['name']))
 							<h2>Contact Us</h2>
 						</div>
 						<div class="art-content">
-							<!--Warning-->
-							<center><?php echo $text;?></center>
+							
 							<!---->
 							<div id="contact_form">
 								<form name="form1" id="ff" method="post" action="contact.php">
@@ -160,62 +129,10 @@ if(isset($_POST['name']))
 				</div>
 				<div id="sidebar" class="col-md-4">
 					<div class="widget wid-about">
-						<div class="heading"><h4>Shortly About Me</h4></div>
+						<div class="heading"><h4>Shortly About 1SMILE</h4></div>
 						<div class="content">
-							<img src="images/20.jpg" />
-							<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-						</div>
-					</div>
-					<div class="widget wid-tags">
-						<div class="heading"><h4>Tag</h4></div>
-						<div class="content">
-							<ul class="list-inline">
-								<li><a href="#">Lorem</a></li>
-								<li><a href="#">Sadipscing</a></li>
-								<li><a href="#">Consetetur</a></li>
-								<li><a href="#">Voluptua</a></li>
-								<li><a href="#">Dolore</a></li>
-								<li><a href="#">Travel</a></li>
-								<li><a href="#">Video</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="widget wid-posts">
-						<div class="heading"><h4>Popular Posts</h4></div>
-						<div class="content">
-							<ul class="list-inline">
-								<div class="post">
-									<a href="#"><img src="images/1.jpg"/></a>
-									<div class="wrapper">
-									  <h5><a href="#">A Blue Morning</a></h5>
-									  <p>March 1, 2015</p>
-									</div>
-								</div>
-								<div class="post">
-									<a href="#"><img src="images/2.jpg"/></a>
-									<div class="wrapper">
-									  <h5><a href="#">A Blue Morning</a></h5>
-									  <p>March 1, 2015</p>
-									</div>
-								</div>
-								<div class="post">
-									<a href="#"><img src="images/3.jpg"/></a>
-									<div class="wrapper">
-									  <h5><a href="#">A Blue Morning</a></h5>
-									  <p>March 1, 2015</p>
-									</div>
-								</div>
-							</ul>
-						</div>
-					</div>
-					<div class="widget wid-archive">
-						<div class="heading"><h4>Archive</h4></div>
-						<div class="content">
-							<select>
-							    <option value="audi" selected>Select Month</option>
-								<option value="volvo">March 2015</option>
-								<option value="saab">Febuary 2015</option>
-							</select>
+							<img src="images/thomas.png" />
+							<p>.</p>
 						</div>
 					</div>
 					<div class="widget wid-gallery">
@@ -240,46 +157,44 @@ if(isset($_POST['name']))
 							</div>
 						</div>
 					</div>
-					<div class="widget wid-links">
-						<div class="heading"><h4>Categories</h4></div>
-						<div class="content">
-							<ul>
-								<li><a href="#">> Lorem ipsum dolor sit </a></li>
-								<li><a href="#">> Nullam venenatis lacus a </a></li>
-								<li><a href="#">> Morbi ut sapien nec nisl</a></li>
-								<li><a href="#">> Integer a enim ac ex.</a></li>
-								<li><a href="#">> Sed in nunc non eleifend  </a></li>
-								<li><a href="#">> Integer a enim ac ex.</a></li>
-							</ul>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	
 	
 	<!-- /////////////////////////////////////////Footer -->
 	<footer>
 		<div class="wrap-footer">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-4">
-						<span class="copyright">Copyright &copy; Your Website <br/> <a href="http://www.365bootstrap.com" target="_blank" rel="nofollow">Bootstrap Themes</a> by 365Bootstrap.com</span>
+					<div class="col-md-6">
+						<span class="copyright">Copyright &copy; 2017, 
+						<a href="http://ugphg.sci-gaia.eu" target="_blank" rel="nofollow">Uganda Public Health Gateway.</a> <br>
+						<a href="http://www.sci-gaia.eu" target="_blank" rel="nofollow">Africa Science Gateway</a>,
+						<a href="http://www.tlc.ug" target="_blank" rel="nofollow">
+						TLCGM</a>,
+						<a href="http://www.ecaitours.com" target="_blank" rel="nofollow">ECAI</a>,
+						<a href="http://www.mak.ac" target="_blank" rel="nofollow">Makerere University RK-WHERE Centre</a>
+						</span>
 					</div>
-					<div class="col-md-4">
+					<div class="col-md-3">
 						<ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
+							<li>Sign in with:</a></li><br>
+							<li><a href="#"><i class="fa fa-twitter fa-2x"></i></a>
 							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
+							<li><a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
 							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
+							<li><a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
+							</li>
+							<li><a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
 							</li>
 						</ul>
 					</div>
-					<div class="col-md-4">
+					<div class="col-md-3">
 						<ul class="list-inline quicklinks">
-							<li><a href="#">Privacy Policy</a>
+							<li><a href="#">Honor Code</a>
+							</li>
+							<li><a href="#">Privacy</a>
 							</li>
 							<li><a href="#">Terms of Use</a>
 							</li>
@@ -290,6 +205,7 @@ if(isset($_POST['name']))
 		</div>
 	</footer>
 	<!-- Footer -->
+	
 	
 	<!-- Top Slider -->
     <script src="js/jquery.js"></script>
